78f84337f41391286c89a1ee5476438268784815
altalk23 <altalk.23@robcol.k12.tr>
2020-02-23 12:05:01 +0300

    solutions start

--- lib/solution/solution.dart
+++ lib/solution/solution.dart
@@ -1,9 +1,70 @@
+import 'dart:collection';
 
 import 'package:parabola_guide/item/item.dart';
+import 'package:parabola_guide/item/item_type.dart';
+import 'package:parabola_guide/solution/type.dart';
 
 class Solver {
     final List<Item> items;
-    Solver(this.items) {
     
+    Solver(this.items) {
+        HashSet<ItemType> type = HashSet.from(items);
+        SolutionType.values.forEach((element) {
+            if (element.isThis(type)) {
+                switch (element) {
+                  case SolutionType.FindRootsFromFactoredForm:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindRootsFromVertexForm:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindRootsFromStandardFormUsingFactoring:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindRootsFromStandardFormUsingCompletingTheSquare:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindRootsFromStandardFormUsingDiscriminant:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindSumAndProductOfRoots:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindVertexOfEquation:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindEquationWithGivenTwoRootsAndOnePoint:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindEquationWithGivenVertexAndOnePoint:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindEquationWithGivenOneRootAndTwoPoints:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindEquationWithGivenThreePoints:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindDiscriminantOfEquation:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindIntersectionsOfParabolaAndLine:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindIntersectionsOfTwoParabolas:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.SketchTheVertexFormLabelingImportantPoints:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.SketchTheFactoredFormLabelingImportantPoints:
+                    // TODO: Handle this case.
+                    break;
+                  case SolutionType.FindSumOfRootsWithGivenVertex:
+                    // TODO: Handle this case.
+                    break;
+                }
+            }
+        });
     }
 }
